32d3e9456a6b7e4802fb86d3bd93dcfab307a3e2
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2017-09-06 05:05:19 +0200

    Update run_analysis.R
    Update to provide narrow and wide tidy data.
    Update to reflect assignment instructions with data file.
    Update to follow assignment instruction order.

--- run_analysis.R
+++ run_analysis.R
@@ -1,69 +1,84 @@
-#Check if dir for data exists, create if not.
-if(!dir.exists(paste(getwd(), "/Data", sep = ""))){
-        dir.create("./Data")
-}
-setwd(paste(getwd(), "/Data", sep = ""))
-
-#download.file with method = "curl" did not work for link. This package works.
-if(!require(downloader)){
-        install.packages("downloader")
-        library(downloader)
+#requires packages
+if(!require(plyr)){
+        install.packages("plyr")
+        library(plyr)
 }
 
-#the readr package is good for loading the data into list
-if(!require(readr)){
-        install.packages("readr")
-        library(readr)
-}
-
-#The dplyr package is just so nice :)
 if(!require(dplyr)){
         install.packages("dplyr")
         library(dplyr)
 }
 
-#This part downloads the necessary data file.
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-dest <- getwd()
-download(fileUrl, dest, mode = "wb")
+if(!require(reshape2)){
+        install.packages("reshape2")
+        library(reshape2)
+}
 
-#Unzip and load relevant files
+#Assignment instructions assume that we have data file in wd
+#Unzip and load relevant files into data frames
 unzip("S4data.zip")
-xtestf <- "./UCI HAR Dataset/test/X_test.txt"
-xtrainf <- "./UCI HAR Dataset/train/X_train.txt"
-featuresf <- "./UCI HAR Dataset/features.txt"
-activitylabelsf <- "./UCI HAR Dataset/activity_labels.txt"
-testsubjectf <- "./UCI HAR Dataset/test/subject_test.txt"
-trainsubjectf <- "./UCI HAR Dataset/train/subject_train.txt"
-ytestf <- "./UCI HAR Dataset/test/y_test.txt"
-ytrainf <- "./UCI HAR Dataset/train/y_train.txt"
 
-#create a tibble for each file
-x_test <- tbl_df(read.csv(xtestf, sep = "", header = FALSE))
-x_train <- tbl_df(read.csv(xtrainf, sep = "", header = FALSE))
-features_tbl <- tbl_df(read.csv(featuresf, sep = "", header = FALSE))
-activity_labels <- tbl_df(read.csv(activitylabelsf, sep = "", header = FALSE))
-test_subject <- tbl_df(read.csv(testsubjectf, sep = "", header = FALSE))
-train_subject <- tbl_df(read.csv(trainsubjectf, sep = "", header = FALSE))
-y_test <- tbl_df(read.csv(ytestf, sep = "", header = FALSE))
-y_train <- tbl_df(read.csv(ytrainf, sep = "", header = FALSE))
+x_test <- "./UCI HAR Dataset/test/X_test.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
 
-#create a tibble for subject and activity data
+x_train <- "./UCI HAR Dataset/train/X_train.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
 
-subject_activity_test <- cbind(test_subject, y_test, x_test)
-subject_activity_train <- cbind(train_subject, y_train, x_train)
+features_tbl <- "./UCI HAR Dataset/features.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
 
-#create a vector containing the column names and pass it to the two tibbles
-column_names <- c("subject", "activity", as.character(features_tbl[[2]]))
-colnames(subject_activity_test) <- column_names
-colnames(subject_activity_train) <- column_names
+activity_labels <- "./UCI HAR Dataset/activity_labels.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
 
-#create subsetting vector for specified measures
-subsetting_vector <- grepl("^activity|^subject|mean|std", column_names,
-                           ignore.case = TRUE)
+subject_test <- "./UCI HAR Dataset/test/subject_test.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
 
-#I remove columns first (too save object.size) and then combine the tibbles
-subject_activity_test_sub <- subject_activity_test[, subsetting_vector]
-subject_activity_train_sub <- subject_activity_train[, subsetting_vector]
-untidy_data <- rbind(subject_activity_test_sub, subject_activity_train_sub)
-tidy_data <- summarize_all(group_by(untidy_data, "subject", "activity"), mean)
+subject_train <- "./UCI HAR Dataset/train/subject_train.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
+y_test <- "./UCI HAR Dataset/test/y_test.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
+
+y_train <- "./UCI HAR Dataset/train/y_train.txt" %>% 
+        read.csv(sep = "", header = FALSE) %>% 
+        tbl_df()
+
+#FROM HERE ON THE SCRIPT FOLLOWS THE ASSIGNMENT STEPS
+#1. Merges the training and the test sets to create one data set.
+merged_data <- rbind(x_train, x_test) %>% 
+        cbind(rbind(y_train, y_test), .) %>%
+        cbind(rbind(subject_train, subject_test), .)
+
+#2. Extracts only the measurements on the mean and std for each measurement.
+extr_df <- as.character(features_tbl[[2]]) %>% 
+        c("subject", "activity", .) %>%
+        grepl("^activity|^subject|mean|std", ., ignore.case = TRUE) %>%
+        merged_data[, .]
+
+#3. Uses descriptive activity names to name the activities in the data set
+extr_df[, 2] <- extr_df[, 2] %>%
+        mapvalues(from = 1:6, to = as.character(activity_labels[[2]][1:6]))
+        
+#4. Appropriately labels the data set with descriptive variable names.
+colnames(extr_df) <- 
+        as.character(features_tbl[[2]]) %>%
+        grepl("mean|std", ., ignore.case = TRUE) %>%
+        as.character(features_tbl[[2]])[.] %>%
+        c("subject", "activity", .)
+
+#5. From the data set in step 4, creates a second, independent tidy data set
+#with the average of each variable for each activity and each subject.
+#n.b. the script creates two tidy files, one wide and one narrow
+tidy_data_wide <- extr_df %>%
+        group_by(subject, activity) %>%
+        summarize_all(mean)
+
+tidy_data_narrow <- tidy_data_wide %>%
+        melt(id = c("subject", "activity"), variable.name = "feature") %>% 
+        arrange(subject, activity)
